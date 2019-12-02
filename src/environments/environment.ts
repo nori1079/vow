e39a8b2eba277b5d0775bf3ad8c34007ab6689b0
yoshinori <koro1079@yahoo.co.jp>
2019-12-02 20:00:35 +0900

    初期設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDk3mOVIZLGmYHhAMxv4F-7NcN37kIkssA',
+    authDomain: 'vow-c81e4.firebaseapp.com',
+    databaseURL: 'https://vow-c81e4.firebaseio.com',
+    projectId: 'vow-c81e4',
+    storageBucket: 'vow-c81e4.appspot.com',
+    messagingSenderId: '1045677471601',
+    appId: '1:1045677471601:web:928d6d43c04a83e7fd28d0',
+    measurementId: 'G-QYJHFZ3PKV'
+  }
 };
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
