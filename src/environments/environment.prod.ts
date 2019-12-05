1b9b43ddb01e91f371fa54ba4a9fb649e30a56eb
yoshinori <koro1079@yahoo.co.jp>
2019-12-05 16:41:42 +0900

    アクセス kー追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBzx48aG35j6siJLw0r6WuTKqoEGGq_w1E',
+    authDomain: 'voww-8f5ba.firebaseapp.com',
+    databaseURL: 'https://voww-8f5ba.firebaseio.com',
+    projectId: 'voww-8f5ba',
+    storageBucket: 'voww-8f5ba.appspot.com',
+    messagingSenderId: '478654840485',
+    appId: '1:478654840485:web:5df3cb13d2e284d632974c',
+    measurementId: 'G-65SGT5DTBQ'
+  }
 };
